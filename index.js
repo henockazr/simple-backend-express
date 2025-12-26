30c42cdc4a9910f54f0d995f07219656f1fcfe3f
henockazr <azrielhenock@yahoo.com>
2025-12-26 09:32:10 +0700

    Prepare for Deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import studentRoutes from './routes/students.js';
 import authRoutes from './routes/auth.js'
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json()); 
