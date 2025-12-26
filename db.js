694ce71642da1f2d3a29f5f3edfc7c653c97842d
henockazr <azrielhenock@yahoo.com>
2025-12-26 09:46:02 +0700

    Fix: Update DB Connection for Cloud

--- db.js
+++ db.js
@@ -3,13 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-});
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? { connectionString: process.env.DATABASE_URL }
+        : {
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+        }
+);
 
 pool.connect((err) => {
     if (err) {
